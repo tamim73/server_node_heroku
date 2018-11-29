15d3b087166fe4aba2168dbaabcfc829084fadea
tamim73 <43931225+tamim73@users.noreply.github.com>
2018-11-29 10:42:36 +0200

    update server for angular myApp

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyparser.json());
 var db = require('knex')({
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE_URL,
+        connectionString: process.env.DATABASE_URL|| 'postgres://nncldsyuqxicbq:91b70146100afed46629f1ce10bbabba3289ac7086811b318115478e3cd02e3e@ec2-54-243-147-162.compute-1.amazonaws.com:5432/df524f83132njv',
         ssl: true,
     }
 });
@@ -20,19 +20,57 @@ var db = require('knex')({
 //midware
 app.use(cors());
 
-
+function verifyToken(req,res,next){
+    if (!req.headers.authorization){
+        return res.status(401).send('Unauthorized request, sent from server verifyToken method')
+    }
+    let token = req.headers.authorization.split(' ')[1]
+    if ( token === 'null') {
+        return res.status(401).send('Unauthorized request, sent from server verifyToken method')
+    }
+    let payload = jwt.verify(token, 'secretKey')
+    if (!payload) {
+        return res.status(401).send('Unauthorized request, sent from server verifyToken method')
+    }
+    req.userId = payload.subject
+    next()
+}
 
 //.........root
 app.get('/', (req, res) => {
     res.json("home")
 })
 
+//.........token verify
+app.post('/v', (req, res) => {
+    const {token} = req.body
+    const payload = jwt.verify(token, 'secretKey' )
+    console.log(payload);
+    if (!payload) {
+        return res.status(401).send(false)
+    }
+    res.status(200).send(true)
+})
 //.........signin
 app.post('/signin', (req, res) => {
-    db.select().from('login').where('email',req.email)
+    const   {email,hash} = req.body
+    db('login').where({email})
     .then(user => {
-        res.status(200).send(user)
+        user = user[0]
+        console.log(user);
+        if (!user){
+            res.status(401).json('Invalid email')
+        } else {
+            if (user.hash!==hash) {
+                res.status(401).json('Invalid password')
+            } else {
+                let payload = { subject: user.id }
+                let token = jwt.sign(payload, 'secretKey')
+                res.status(200).send({token})
+            }
+        }
     })
+    .catch(err=> res.status(400).json('cannot login'))
 })
 //!......... register 
 app.post('/register', (req, res) => {
@@ -44,7 +82,7 @@ app.post('/register', (req, res) => {
         let token = jwt.sign(payload, 'secretKey')
         res.status(200).send({token});
     })
-    .catch(err => res.status(400).json('cant register'))
+    .catch(err => res.status(400).send('cannot register'))
 })
 
 //!.........deparments
@@ -166,4 +204,6 @@ app.delete('/emps/:id', (req, res) => {
 
 
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('server is running');
+});
